a29e0e15f9bb026424e527d701be1d2a4e96af62
Jiaxin-Chen <jiaxinc@andrew.cmu.edu>
2017-05-17 17:26:05 +0800

    update sum

--- Fighting/LC015.java
+++ Fighting/LC015.java
@@ -62,6 +62,60 @@ public class LC015{
 		return res;
 	}
 
+
+	// Worse Version:
+	// Time Complexity: O(N^2)
+	// Runtime: 107ms, beat 28.49%
+	public List<List<Integer>> threeSum2(int[] nums){
+
+		List<List<Integer>> res = new ArrayList<>();
+		if(nums == null || nums.length <= 2){
+			return res;
+		}
+
+		Arrays.sort(nums);
+		for(int i = 0; i < nums.length - 2; i++){
+
+			// DONN'T MISS IT!
+			// Skip the duplicate triplets results
+			if(i > 0 && nums[i] == nums[i-1]){
+				continue;
+			}
+
+			int left = i + 1, right = nums.length - 1;
+			int sum = 0 - nums[i];
+			while(left < right){
+				if(nums[left] + nums[right] == sum){
+					res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+					left++;
+					right--;
+					// Skip the duplicate triplets results
+					while(left < right && nums[left] == nums[left-1]){
+						left++;
+					}
+					while(left < right && nums[right] == nums[right+1]){
+						right--;
+					}
+				}else if(nums[left] + nums[right] < sum){
+					left++;
+					// Try to optimize to skip the duplicate triplets results, but maybe add too many loops and conditions...
+					while(left < right && nums[left] == nums[left-1]){
+						left++;
+					}
+				}else{
+					right--;
+					// Try to optimize to skip the duplicate triplets results, , but maybe add too many loops and conditions...
+					while(left < right && nums[right] == nums[right+1]){
+						right--;
+					}
+				}
+
+			}
+		}
+		return res;
+	}
+
+
 	public static void main(String[] args){
 		int[] nums = {-1, 0, 1, 2, -1, -4};
 		LC015 x = new LC015();
---
+++ Fighting/LC016.java
@@ -0,0 +1,90 @@
+/*
+Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. 
+Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+Example:
+	Given array S = {-1 2 1 -4}, and target = 1.
+    The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ */
+import java.util.Arrays;
+
+public class LC016{
+
+	// Time Complexity: O(N^2)
+	// Runtime: 39ms, beat 4.17%, so slow!!!!!
+	public int threeSumClosest(int[] nums, int target){
+		
+		if(nums == null || nums.length < 3){
+			return 0;
+		}
+		int res = nums[0] + nums[1] + nums[nums.length - 1];
+		Arrays.sort(nums);
+		for(int i = 0; i < nums.length - 2; i++){
+			
+			int left = i + 1, right = nums.length - 1;
+			while(left < right){
+				int sum = nums[i] + nums[left] + nums[right];
+				if(Math.abs(sum - target) < Math.abs(res - target)){
+					res = sum;
+				}
+				if(sum > target){
+					right--;
+				}else{
+					left++;
+				}
+			}
+		}
+		return res;
+	}
+
+	// Optimized Version:
+	// Time Complexity: O(N^2)
+	// Runtime: 20ms, beat 88.78%
+	public int threeSumClosest2(int[] nums, int target){
+		if(nums == null || nums.length < 3){
+			return 0;
+		}
+		Arrays.sort(nums);
+		int res = nums[0] + nums[1] + nums[nums.length - 1];
+		for(int i = 0; i < nums.length - 2; i++){
+			// Skip the duplicate triplets results
+			if(i == 0 || nums[i] != nums[i-1]){
+				int left = i + 1, right = nums.length - 1;
+				while(left < right){
+					int sum = nums[i] + nums[left] + nums[right];
+					if(sum > target){
+						right--;
+						// Skip the duplicate triplets results
+						while(left < right && nums[right] == nums[right+1]){
+							right--;
+						}
+					}else if(sum < target){
+						left++;
+						// Skip the duplicate triplets results
+						while(left < right && nums[left] == nums[left-1]){
+							left++;
+						}
+					}else{
+						return sum;
+					}
+
+					if(Math.abs(sum - target) < Math.abs(res - target)){
+						res = sum;
+					}
+				}
+			}
+		}
+		return res;
+	}
+
+
+
+	public static void main(String[] args){
+		int[] nums = {1, 1, 1, 0};
+		int target = 100;
+
+		LC016 x = new LC016();
+		int res = x.threeSumClosest2(nums, target);
+		System.out.println(res);
+	}
+}
